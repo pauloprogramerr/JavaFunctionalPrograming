019d8466db8757873180ddc37869c38f60534a79
pauloprogramerr <paulo_pauladas@hotmail.com>
2025-04-10 15:52:04 -0300

    Estudos lambdas expressions and reference method pag 80

--- src/main/java/com/funcional/Main.java
+++ src/main/java/com/funcional/Main.java
@@ -2,6 +2,6 @@ package com.funcional;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World! teste");
+        System.out.println("Hello, World!");
     }
 }
